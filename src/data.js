461b5c5ae463799644e29f09b13441db9c9c363f
TimothyMcC <tmkgw@live.com>
2023-08-14 16:02:17 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Portfolio Site",
+    subtitle: "React",
+    description:
+      "One of my first ever projects, the site you're on right now! Created in React using a combination of JavaScript and HTML/CSS, this portfolio should showcase all of my skills/accomplishments.",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Placeholder Project 1",
+    subtitle: "React and Python",
+    description:
+      "My goal is to have three projects showcased on this site/for my resume, this spot should be filled soon!",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Placeholder Project 2",
+    subtitle: "React and Python",
+    description:
+      "My goal is to have three projects showcased on this site/for my resume, this spot should be filled soon!",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
